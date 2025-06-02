10fe571ec5ca260aaf4931192dce73c4bc11609e
Abhimsa <128863782+Abhimsa@users.noreply.github.com>
2025-06-02 21:32:30 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_vpc" "main_vpc" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "main_subnet" {
+  vpc_id     = aws_vpc.main_vpc.id
+  cidr_block = "10.0.1.0/24"
+}
+
+resource "aws_instance" "web_server" {
+  ami           = "ami-0c94855ba95c71c99" # Amazon Linux 2 in us-east-1
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.main_subnet.id
+  tags = {
+    Name = "TerraformExample"
+  }
+}
