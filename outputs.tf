240fab29bcfa8baacef4cd2385b30f2c3974be79
Abhimsa <128863782+Abhimsa@users.noreply.github.com>
2025-06-02 22:11:50 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,14 @@
 output "vpc_id" {
-  value = aws_vpc.main_vpc.id
+  description = "The ID of the VPC"
+  value       = aws_vpc.main_vpc.id
+}
+
+output "subnet_id" {
+  description = "The ID of the subnet"
+  value       = aws_subnet.main_subnet.id
 }
 
 output "instance_id" {
-  value = aws_instance.web_server.id
+  description = "The ID of the EC2 instance"
+  value       = aws_instance.web_server.id
 }
