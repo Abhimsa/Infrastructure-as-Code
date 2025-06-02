015b8fac7a25afc529a7cf54b0e988644ba9abaf
Abhimsa <128863782+Abhimsa@users.noreply.github.com>
2025-06-02 22:11:06 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,21 +1,31 @@
 provider "aws" {
-  region = "us-east-1"
+  region = var.region
 }
 
 resource "aws_vpc" "main_vpc" {
   cidr_block = "10.0.0.0/16"
+
+  tags = {
+    Name = "main-vpc"
+  }
 }
 
 resource "aws_subnet" "main_subnet" {
-  vpc_id     = aws_vpc.main_vpc.id
-  cidr_block = "10.0.1.0/24"
+  vpc_id            = aws_vpc.main_vpc.id
+  cidr_block        = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name = "main-subnet"
+  }
 }
 
 resource "aws_instance" "web_server" {
-  ami           = "ami-0c94855ba95c71c99" # Amazon Linux 2 in us-east-1
+  ami           = "ami-0c94855ba95c71c99" # Amazon Linux 2 (us-east-1)
   instance_type = "t2.micro"
   subnet_id     = aws_subnet.main_subnet.id
+
   tags = {
-    Name = "TerraformExample"
+    Name = "web-server"
   }
 }
